40229535e8411c6fc7807a57362ca764327be64e
Eva Reitzel <evareitzel@gmail.com>
2022-05-24 01:05:24 -0400

    commit tree 4eca5f47adb6df77d8d7553e427ac93068cb948e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+// element, index, array
+
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === 'W' );
+    return !!result ? result.year : undefined
+}
+
+// superbowlWin = (record) => {
+//     const result = record.find
+//     // ( record => record.result === 'W' );
+//     (function(record){
+//         record.result === 'W';
+//     })
+//     if ( result ){
+//         return result.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+// (arg) => obj.result === 'W' => return obj.year; 
+    // iterate thru record object
+    // use .find() to return "W" result year
+
+// Notes
+// Arrow function x2
+// Complex problems breakdown into simple solutions
